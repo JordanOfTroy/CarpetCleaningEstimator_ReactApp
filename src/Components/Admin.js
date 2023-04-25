65d2fbbb94be7c97cd0a822cb339e4b5b9d52602
Jordan <jordan@jordans-air.lan>
2023-04-24 21:47:06 -0600

    admin page shoing rooms, sft and est total

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,20 +1,40 @@
+import { useState } from 'react';
 import '../styles/admin.css'
 import EstimateCard from "./EstimateCard";
 
 export default function Admin () {
 
+    let [update, setUpdate] = useState(false)
+
+    function deleteEstimate (i) {
+        let estimates = JSON.parse(localStorage.getItem('estimates'))
+        console.log('index:', i)
+        console.log('before:', estimates)
+        estimates.splice(i, 1)
+        console.log('after:', estimates)
+        localStorage.setItem('estimates', JSON.stringify(estimates))
+        setUpdate(!update)
+    }
+
     function showAllEstimate () {
         let results = []
         let estimates = JSON.parse(localStorage.getItem('estimates'))
-        estimates.forEach(estimate => {
-            results.push(<EstimateCard estimate = {estimate}/>)
+        estimates.forEach((estimate, i) => {
+            
+            results.push(
+                <EstimateCard 
+                    estimate = {estimate}
+                    i = {i}
+                    deleteEstimate = {deleteEstimate}
+                />
+            )
         });
         return results
     }
 
     return (
-        <div>
-            <div className='adminHeader'>
+        <div className='adminContainer'>
+            {/* <div className='adminHeader'>
                 <div className='headerItem'>
                     <p>Price per square foot: $1</p>
                     <button>Update Pricing</button>
@@ -29,7 +49,7 @@ export default function Admin () {
                         <option value='zFirst'>Name: Z to A</option>
                     </select>
                 </div>
-            </div>
+            </div> */}
             <div className="cardBox">
                 {showAllEstimate()}
             </div>
--- src/Components/EstimateCard.js
+++ src/Components/EstimateCard.js
@@ -1,7 +1,15 @@
 import '../styles/estimatecard.css'
 
-export default function EstimateCard ({estimate}) {
+export default function EstimateCard ({estimate, i, deleteEstimate}) {
     let {firstName, lastName, uuid, telephone, email, areas, date} = estimate
+
+    let grandTotal = 0
+
+    areas.forEach((area) => {
+        let {roomLength, roomWidth} = area
+        let roomTotal = roomLength * roomWidth
+        grandTotal += roomTotal
+    })
 
     return (
         <div className="card">
@@ -13,10 +21,13 @@ export default function EstimateCard ({estimate}) {
                 <p>phone: {telephone}</p>
                 <p>email: {email}</p>
                 <p>number of rooms: {areas.length}</p>
-                <p>estimate total: $100</p>
+                <ul className='roomUL'>
+                    {areas.map((area, i) => <li key={i}>{area.roomName} <span className='lowLite'>({area.roomLength} x {area.roomWidth})</span></li>)}
+                </ul>
+                <p>Estimate Total: <span className='greenMe'>${grandTotal}</span></p>
             </div>
             <div className="cardFooter">
-                <button>Delete Estimate</button>
+                <button onClick={() => deleteEstimate(i)}>Delete Estimate</button>
             </div>
         </div>
     )
