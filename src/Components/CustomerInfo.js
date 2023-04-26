ad7c85adc52e5cf1a2ed2c05d2898a4b37f7c5de
Jordan <jordan@Jordans-MacBook-Air.local>
2023-04-26 06:58:28 -0600

    final commit for grad submission

--- src/Components/CustomerInfo.js
+++ src/Components/CustomerInfo.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom"
-import {v4 as uuidv4} from 'uuid'
+import {v4 as uuidv4} from 'uuid' 
 
 export default function CustomerInfo ({StartNewEstimate}) {
     let navigate = useNavigate()
